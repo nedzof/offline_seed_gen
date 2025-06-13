a17ac571cda2756a8322bc0441f905fa878c2628
nedzof <nedzof@users.noreply.github.com>
2025-06-13 16:10:03 +0200

    Fix P2PKH address generation: Use derive_child instead of child_key method

--- main.py
+++ main.py
@@ -523,15 +523,15 @@ def generate_p2pkh_addresses(mnemonic: str, passphrase: str, derivation_path: st
         # Derive the path
         for part in path_parts[1:]:  # Skip 'm'
             if part.endswith("'"):
-                current_key = current_key.child_key(int(part[:-1]), is_hardened=True)
+                current_key = current_key.derive_child(int(part[:-1]), is_hardened=True)
             else:
-                current_key = current_key.child_key(int(part))
+                current_key = current_key.derive_child(int(part))
         
         # Generate addresses
         addresses = []
         for i in range(count):
             # Derive child key for each address
-            child_key = current_key.child_key(i)
+            child_key = current_key.derive_child(i)
             # Get public key
             public_key = child_key.public_key
             # Generate P2PKH address
